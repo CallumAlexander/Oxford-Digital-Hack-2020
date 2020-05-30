d7857bca3dd0a448f92166d8f7e4e66b0243e4ee
Cal Alexander <s1931801@ed.ac.uk>
2020-05-30 23:12:15 +0100

    fixed somethin

--- web/query.php
+++ web/query.php
@@ -3,7 +3,7 @@
     $client = new MongoDB\Client(
         'mongodb+srv://kay:ImABigFanny@cluster0.mongodb.net/?ssl=true&authSource=admin&serverSelectionTryOnce=false&serverSelectionTimeoutMS=15000&w=majority"');
 
-    $db = $client->test;
+    $db = $client->friendr;
 
     echo "This thing worked";
 
