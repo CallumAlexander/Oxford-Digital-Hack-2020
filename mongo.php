178c2a676497a783065b2ae4fb15dfcace01e69b
Cal Alexander <s1931801@ed.ac.uk>
2020-05-30 17:51:30 +0100

    created simple mongo.php script

---
+++ mongo.php
@@ -0,0 +1,8 @@
+<?php
+
+    $client = new MongoDB\Client(
+        'mongodb+srv://cal:ImABigFanny@cluster0-eodrt.gcp.mongodb.net/test?retryWrites=true&w=majority');
+
+    $db = $client->test;
+
+?>
